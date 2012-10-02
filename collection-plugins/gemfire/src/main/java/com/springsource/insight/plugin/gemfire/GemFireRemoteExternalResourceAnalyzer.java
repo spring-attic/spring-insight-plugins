a2549ffb422d56335d868b7417e5a008a17c37b3
Shachar Anchelovich <shachar@vmware.com>
2012-10-02 06:05:08 +0200

    METRICS-3026: As a user I would like to see external resources for embedded gemfire

--- collection-plugins/gemfire/src/main/java/com/springsource/insight/plugin/gemfire/GemFireRemoteExternalResourceAnalyzer.java
+++ collection-plugins/gemfire/src/main/java/com/springsource/insight/plugin/gemfire/GemFireRemoteExternalResourceAnalyzer.java
@@ -16,96 +16,33 @@
 
 package com.springsource.insight.plugin.gemfire;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
 import com.springsource.insight.intercept.operation.Operation;
-import com.springsource.insight.intercept.topology.AbstractExternalResourceAnalyzer;
-import com.springsource.insight.intercept.topology.ExternalResourceDescriptor;
-import com.springsource.insight.intercept.topology.ExternalResourceType;
-import com.springsource.insight.intercept.topology.MD5NameGenerator;
 import com.springsource.insight.intercept.trace.Frame;
-import com.springsource.insight.intercept.trace.Trace;
-import com.springsource.insight.util.ListUtil;
 import com.springsource.insight.util.StringUtil;
 
-public class GemFireRemoteExternalResourceAnalyzer extends AbstractExternalResourceAnalyzer {
-	private static final int EMPTY_PORT = -1;
-
-    public GemFireRemoteExternalResourceAnalyzer () {
+public class GemFireRemoteExternalResourceAnalyzer extends AbstractGemFireExternalResourceAnalyzer {
+	public GemFireRemoteExternalResourceAnalyzer () {
 		super(GemFireDefenitions.TYPE_REMOTE.getType());
 	}
 
-	public Collection<ExternalResourceDescriptor> locateExternalResourceName(Trace trace, Collection<Frame> frames) {
-		if (ListUtil.size(frames) <= 0) {
-		    return Collections.emptyList();
-		}
-
-		List<ExternalResourceDescriptor> descriptors = new ArrayList<ExternalResourceDescriptor>(frames.size());
-		for (Frame frame : frames) {			
-            Operation       op = frame.getOperation();
-            
-            String hostname = op.get(GemFireDefenitions.FIELD_HOST, String.class);
-            Number portObj = op.get(GemFireDefenitions.FIELD_PORT, Number.class);
-            int port = portObj != null ? portObj.intValue() : EMPTY_PORT;
-            
-			if (StringUtil.isEmpty(hostname) || GemFireDefenitions.FIELD_UNKNOWN.equals(hostname)) {
-				continue;
-			}
-
-            String color = colorManager.getColor(op);
-			String label = createLabel(hostname, port);
-			String name = createName(hostname, port, label);
-			
-			ExternalResourceDescriptor desc =
-			        new ExternalResourceDescriptor(frame,
-			                                       name,
-			                                       label,
-			                                       ExternalResourceType.KVSTORE.name(),
-			                                       GemFireDefenitions.GEMFIRE,
-			                                       hostname,
-			                                       port,
-			                                       color,
-			                                       false);
-			descriptors.add(desc);
-		}
-		
-		return descriptors;
+	@Override
+    protected String getHostname(Operation op) {
+	    return op.get(GemFireDefenitions.FIELD_HOST, String.class);
 	}
 	
-	public final static String createLabel(String hostname, int port) {
-	    StringBuilder builder = new StringBuilder((hostname != null ? hostname.length() : 4) + 6);
-	    
-	    builder.append(hostname);
-	    builder.append(':');
-	    builder.append(port);
-	    
-	    return builder.toString();
+	@Override
+    protected int getPort(Operation op) {
+	    Number portObj = op.get(GemFireDefenitions.FIELD_PORT, Number.class);
+        int port = portObj != null ? portObj.intValue() : EMPTY_PORT;
+        
+        return port;
 	}
 	
-	public final static String createName(String hostname, int port) {
-	    return createName(hostname, port, null);
-	}
-	
-	public final static String createName(String hostname, int port, String label) {
-	    String labelToUse = label != null ? label : createLabel(hostname, port);
-	    StringBuilder builder = new StringBuilder(GemFireDefenitions.GEMFIRE.length() + 1 + labelToUse.length());
-	    
-	    builder.append(GemFireDefenitions.GEMFIRE);
-	    builder.append(':');
-	    builder.append(labelToUse);
-	    
-	    String name = MD5NameGenerator.getName(builder.toString());
-	    
-	    builder.delete(0, builder.length());
-	    builder.ensureCapacity(GemFireDefenitions.GEMFIRE.length() + 1 + name.length());
-	    
-	    builder.append(GemFireDefenitions.GEMFIRE);
-	    builder.append(':');
-	    builder.append(name);
-	    
-	    return builder.toString();
+	@Override
+    protected boolean shouldCreateExteranlResource(Frame frame) {
+	    Operation op = frame.getOperation();
+	    String hostname = getHostname(op);
+	    return !StringUtil.isEmpty(hostname) && !GemFireDefenitions.FIELD_UNKNOWN.equals(hostname);
 	}
 }
---
+++ collection-plugins/gemfire/src/test/java/com/springsource/insight/plugin/gemfire/AbstractGemFireExternalResourceAnalyzerTestSupport.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2009-2011 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.springsource.insight.plugin.gemfire;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.springsource.insight.collection.test.AbstractCollectionTestSupport;
+import com.springsource.insight.intercept.application.ApplicationName;
+import com.springsource.insight.intercept.operation.Operation;
+import com.springsource.insight.intercept.server.ServerName;
+import com.springsource.insight.intercept.topology.ExternalResourceDescriptor;
+import com.springsource.insight.intercept.topology.ExternalResourceType;
+import com.springsource.insight.intercept.trace.Frame;
+import com.springsource.insight.intercept.trace.FrameId;
+import com.springsource.insight.intercept.trace.SimpleFrame;
+import com.springsource.insight.intercept.trace.Trace;
+import com.springsource.insight.intercept.trace.TraceId;
+import com.springsource.insight.util.time.TimeRange;
+
+public abstract class AbstractGemFireExternalResourceAnalyzerTestSupport extends AbstractCollectionTestSupport  {
+
+    final AbstractGemFireExternalResourceAnalyzer analyzer;
+    
+    protected AbstractGemFireExternalResourceAnalyzerTestSupport(AbstractGemFireExternalResourceAnalyzer analyzer) {
+        this.analyzer = analyzer;
+    }
+    
+	@Test
+	public void testWithoutRemoteFrame() throws Exception {
+		Operation op = new Operation();		
+		op.type(analyzer.getOperationType());
+		
+		Frame frame = new SimpleFrame(FrameId.valueOf("0"),
+				null,
+				op,
+				TimeRange.milliTimeRange(0, 1),
+				Collections.<Frame>emptyList());
+
+		Trace trace = new Trace(ServerName.valueOf("fake-server"),
+				ApplicationName.valueOf("fake-app"),
+				new Date(),
+				TraceId.valueOf("fake-id"),
+				frame);
+		
+		Collection<ExternalResourceDescriptor>    res=analyzer.locateExternalResourceName(trace);
+		assertEquals("Mismatched number of results", 1, res.size());
+		ExternalResourceDescriptor externalResourceDescriptor = res.iterator().next();
+
+		assertEquals("Gemfire source frame", frame, externalResourceDescriptor.getFrame());
+		assertNull("Gemfire external resource host", externalResourceDescriptor.getHost());
+		assertEquals("Gemfire external resource port", -1, externalResourceDescriptor.getPort());
+		assertEquals("Gemfire external resource type", ExternalResourceType.KVSTORE.name(), externalResourceDescriptor.getType());
+		assertEquals("Gemfire external resource type", ExternalResourceType.KVSTORE.name(), externalResourceDescriptor.getType());
+		assertEquals("Gemfire external resource name", GemFireRemoteExternalResourceAnalyzer.createName(null, -1), externalResourceDescriptor.getName());
+		assertEquals("Gemfire external resource vendor", GemFireDefenitions.GEMFIRE, externalResourceDescriptor.getVendor());
+		assertEquals("Gemfire external resource label", GemFireRemoteExternalResourceAnalyzer.createLabel(null, -1), externalResourceDescriptor.getLabel());
+		assertEquals("Gemfire external incoming", Boolean.FALSE, Boolean.valueOf(externalResourceDescriptor.isIncoming()));
+	}
+    
+    @Test
+    public void testWithRemoteFrame() throws Exception {
+        Operation op = new Operation();     
+        op.type(analyzer.getOperationType());
+        
+        Frame remoteFrame = new SimpleFrame(FrameId.valueOf("1"),
+                null,
+                new Operation().type(GemFireDefenitions.TYPE_REMOTE.getType()),
+                TimeRange.milliTimeRange(0, 1),
+                Collections.<Frame>emptyList());
+        
+        List<Frame> frames = Collections.singletonList(remoteFrame);
+        
+        Frame frame = new SimpleFrame(FrameId.valueOf("0"),
+                null,
+                op,
+                TimeRange.milliTimeRange(0, 1),
+                frames);
+
+        Trace trace = new Trace(ServerName.valueOf("fake-server"),
+                ApplicationName.valueOf("fake-app"),
+                new Date(),
+                TraceId.valueOf("fake-id"),
+                frame);
+        
+        Collection<ExternalResourceDescriptor>    res=analyzer.locateExternalResourceName(trace);
+        assertEquals("Mismatched number of results", 0, res.size());
+    }
+}
