99518eb631d52426a47cd469cae78fdf40954953
Dave Tillman <dtillman@vmware.com>
2015-02-03 16:37:53 -0700

    More Spring4 updates

---
+++ collection-plugins/spring-integration/src/main/java/com/springsource/insight/plugin/integration/IntegrationPluginRuntimeDescriptor.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2009-2011 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.springsource.insight.plugin.integration;
+
+import com.springsource.insight.intercept.endpoint.EndPointAnalyzer;
+import com.springsource.insight.intercept.plugin.PluginRuntimeDescriptor;
+import com.springsource.insight.util.ArrayUtil;
+
+import java.util.Collection;
+import java.util.List;
+
+public class IntegrationPluginRuntimeDescriptor extends PluginRuntimeDescriptor {
+    public static final String PLUGIN_NAME = "spring-integration";
+    private static final IntegrationPluginRuntimeDescriptor	INSTANCE=new IntegrationPluginRuntimeDescriptor();
+    private static final List<? extends EndPointAnalyzer>	epAnalyzers=ArrayUtil.asUnmodifiableList(IntegrationEndPointAnalyzer.getInstance());
+
+    private IntegrationPluginRuntimeDescriptor() {
+        super();
+    }
+
+    public static final IntegrationPluginRuntimeDescriptor getInstance() {
+    	return INSTANCE;
+    }
+
+    @Override
+    public Collection<? extends EndPointAnalyzer> getEndPointAnalyzers() {
+        return epAnalyzers;
+    }
+
+    @Override
+    public String getPluginName() {
+        return PLUGIN_NAME;
+    }
+}
